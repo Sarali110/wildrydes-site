ff361d4e696ad5289140391bdbd105dbd50a1f24
Sarali110 <32184118+Sarali110@users.noreply.github.com>
2025-10-27 11:29:00 -0700

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_0vRH3DbrZ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6iim2q4sr1rktm8ggn0f2f9kor', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2sok7rca1a2p2da8oe7u2pt2ns', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
