e61779106e3e3c2ee5cf7ae4d04ed9c1e6a522ee
Sarali110 <32184118+Sarali110@users.noreply.github.com>
2025-10-27 11:10:56 -0700

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_0vRH3DbrZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6iim2q4sr1rktm8ggn0f2f9kor', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
